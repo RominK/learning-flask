bba55a29bbc3487d51cced333aeff8267853ba9f
Romin Kayira <Romin Kayira>
2018-08-16 18:26:58 +0300

    address searches

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form 
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class SignupForm(Form):
 	first_name = StringField('First name', validators=[DataRequired("Please enter your First Name")])
@@ -8,3 +8,12 @@ class SignupForm(Form):
 	email = StringField('Email', validators=[DataRequired("Please enter your Email address")])
 	password = PasswordField('Password', validators=[DataRequired("Please enter your Password")])
 	submit = SubmitField('Sign Up')
+
+class LoginForm(Form):
+	email = StringField('Email', validators=[DataRequired("Please Enter correct Email"), Email("Please enter correct email")])
+	password = PasswordField('Password', validators=[DataRequired("Please Enter your password")])
+	submit = SubmitField('Login')
+
+class AddressForm(Form):
+	address = StringField('Address', validators=[DataRequired('Please enter address')])
+	submit = SubmitField('Search')
